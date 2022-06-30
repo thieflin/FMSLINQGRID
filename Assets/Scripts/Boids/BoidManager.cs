3ece9a1cef6273ebdda9eafb3238c58629995a0d
thieflin <ignaciogarrido1234@hotmail.com>
2022-06-30 13:17:38 -0300

    Toque final del skip ya esta para entregar

--- Assets/Scripts/Boids/BoidManager.cs
+++ Assets/Scripts/Boids/BoidManager.cs
@@ -8,7 +8,6 @@ public class BoidManager : MonoBehaviour
 {
     public static BoidManager instance;
     public List<Boid> allBoids = new List<Boid>(); //Lista de los boids
-    public List<Boid> mindWashedBoids = new List<Boid>();
 
     private void Awake()
     {
@@ -23,14 +22,14 @@ public class BoidManager : MonoBehaviour
     }
     private void Start()
     {
-       mindWashedBoids = TraitorBoids(allBoids).ToList();
+       TraitorBoids(allBoids).ToList();
     }
     //El lider enemigo infecto la conciencia de ciertos boids, por lo tanto todos sus lideres (sus primeros 3) y todos sus ultimos reclutas
     //(los ultimos 3) seran secuestrados!
 
     IEnumerable<Boid> TraitorBoids(IEnumerable<Boid> allBoids)
     {
-        var myCol = allBoids.Take(3).Concat(allBoids.Skip(allBoids.Count() - 4))
+        var myCol = allBoids.Take(3).Concat(allBoids.Skip(allBoids.Count() - 3))
         //uNA VEZ Hecha mi primer lista, que son los que son enemigos, lo que hago es ahcer un aggregate y a cada current, osea
         //cada boid enemigo le hago la variable de enemy true y le defino su color
         .Aggregate(new List<Boid>(), (acum, current) =>
